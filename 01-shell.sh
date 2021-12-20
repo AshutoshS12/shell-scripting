63fd2d6443d48549ad811b9b799f965ab4642e0a
saras <saras_ashu@outlook.com>
2021-12-20 22:31:02 +0530

    V1.6.3: Use of Array and Read Command : Correction on Array Declaration as previous code commit didn't work

--- 01-shell.sh
+++ 01-shell.sh
@@ -13,9 +13,9 @@ echo -e "\e[31mLet's Continue\e[0m"
 ## Array
 
 Name=(Ashutosh Asima Taliban)
-${Name [0]};
-${Name [1]};
-${Name [2]};
+echo "${Name [0]}";
+echo "${Name [1]}";
+echo "${Name [2]}";
 echo  "The name of the First Person is ${Name [0]} and Second Person is ${Name [0]},Against of ${Name [0]}"
 
 ##Input--Read Command
