df4701845681ac10881bcdff8d661196bb7b92d8
saras <saras_ashu@outlook.com>
2021-12-20 11:04:48 +0530

    V1.2 : Script to display message and date: using esc sequence

--- 01-shell.sh
+++ 01-shell.sh
@@ -3,9 +3,7 @@
 ## Echo examples
 
 ## Printing Few Messages
-echo Hello User
-echo 'Today is a beautiful day'
-echo "Today's date is $(date) "
+echo -e "Hello User\nToday is a beautiful day\nToday's date is $(date) "
 
 
 
