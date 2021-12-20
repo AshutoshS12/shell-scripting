faa182359b77f176a20b95dcc996e7eef583364c
saras <saras_ashu@outlook.com>
2021-12-20 11:18:14 +0530

    V1.4 : Script to display message and date: using more esc sequence (\t with date)

--- 01-shell.sh
+++ 01-shell.sh
@@ -3,7 +3,7 @@
 ## Echo examples
 
 ## Printing Few Messages
-echo -e "Hello User\nToday is a beautiful day\nToday's date is\t$(date)\nMy Name is Ashutosh Sar "
+echo -e "Hello User\nToday is a beautiful day\nToday's date is\t $(date)\nMy Name is Ashutosh Sar "
 
 ##Colored output
 
