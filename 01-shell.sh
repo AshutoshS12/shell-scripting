cad639cc97c985aacdd63608f04726abc9a808cb
saras <saras_ashu@outlook.com>
2021-12-20 11:43:50 +0530

    V1.5 : Script to display message and date: Adding goodbye message with colored code

--- 01-shell.sh
+++ 01-shell.sh
@@ -6,8 +6,8 @@
 echo -e "Hello User\nToday is a beautiful day\nToday's date is\t $(date)\nMy Name is Ashutosh Sar "
 
 ##Colored output
+##Syntax :- echo -e "\e[COLmMessage" (To enable) , \e[0m (To disable)
 
-
-
+echo -e "\e[31mGoodbye\e[0m"
 
 
