486ddea65b7a425683ceee7866ae4c16d638983e
saras <saras_ashu@outlook.com>
2021-12-20 22:19:42 +0530

    V1.6: Use of Array and Read Command

--- 01-shell.sh
+++ 01-shell.sh
@@ -12,7 +12,7 @@ echo -e "\e[31mLet's Continue\e[0m"
 
 ## Array
 
-Name=('Ashutosh' 'Asima' 'Taliban')
+Name=("Ashutosh" "Asima" "Taliban")
 
 echo -e "The name of the First Person is ${Name [0]} and Second Person is ${Name[1]},Against of ${Name[2]}"
 
