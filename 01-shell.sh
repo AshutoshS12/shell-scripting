18cb0d9db5a49c6a95d2de6c9bdd0b604c1587fe
saras <saras_ashu@outlook.com>
2021-12-20 15:30:42 +0530

    V1.6: Use of Array and Read Command

--- 01-shell.sh
+++ 01-shell.sh
@@ -8,6 +8,23 @@ echo -e "Hello User\nToday is a beautiful day\nToday's date is\t $(date)\nMy Nam
 ##Colored output
 ##Syntax :- echo -e "\e[COLmMessage" (To enable) , \e[0m (To disable)
 
-echo -e "\e[31mGoodbye\e[0m"
+echo -e "\e[31mLet's Continue\e[0m"
+
+## Array
+
+Name=('Ashutosh' 'Asima' 'Taliban')
+
+echo -e "The name of the First Person is ${Name [0]} and Second Person is ${Name[1]},Against of ${Name[2]}"
+
+##Input--Read Command
+
+read -p 'Enter your Name: ' FullName
+
+echo -e "$FullName is a good boy"
+
+
+
+
+
 
 
