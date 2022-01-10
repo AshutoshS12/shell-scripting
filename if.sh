03950c41b18ca63ed373e7bd7ffb2b0a837cf692
saras <saras_ashu@outlook.com>
2022-01-10 12:52:35 +0530

    More Options

--- if.sh
+++ if.sh
@@ -1,7 +1,19 @@
 #!/bib/bash
 
+# shellcheck disable=SC2162
 read -p 'Enter username: ' username
 
+## String Comparison
 if [ "$username" == "root" ] ; then
   echo "Hi , You are a root user"
+else
+  echo -e  "\e[33mHi, You are a non-root user\e[0m"
+  fi
+
+##Number Comparison
+
+if [ "$UID" -eq 0 ] ; then
+  echo "Hi , You are a root user"
+else
+  echo -e  "\e[33mHi, You are a non-root user\e[0m"
   fi
