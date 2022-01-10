843a049bcd3ce0d9d36b77547bf6240aa888fa84
saras <saras_ashu@outlook.com>
2022-01-10 10:14:24 +0530

    V1.12: Modified Script

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,6 +1,19 @@
- yum install nginx -y
 
+ Print() {
+   echo -e "\e[1m$1\e[0m"
+   echo -e "\n\e[31m=================$1=================\e[0m" >> $LOG
+}
+
+ LOG=/tmp/roboshop.log
+ rm -f $LOG
+
+ Print "Installing Nginx"
+ yum install nginx -y &>>$LOG
+
+ Print "Enabling Nginx"
  systemctl enable nginx
+
+ Print "Starting Nginx"
  systemctl start nginx
 
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
